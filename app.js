20d5203f762a5350f0e3f3ab02c82e4c62511b79
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-05-20 09:56:44 +0700

    Finish in JS

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const count = document.querySelector('.count');
 const input = document.createElement('input');
+
+input.addEventListener("keyup", () => {
+    count.innerHTML=input.value.length
+})
